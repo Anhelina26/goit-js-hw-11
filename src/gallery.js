869cd110a2c2b0e8411a26cc3363f2aee2dbb589
Anhelina26 <114609780+Anhelina26@users.noreply.github.com>
2023-11-28 18:33:22 +0100

    add changes

---
+++ src/gallery.js
@@ -0,0 +1,43 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+
+const galleryElement = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+export function createImageCard(image) {
+  const cardHTML = `
+    <div class="photo-card">
+      <a href="${image.largeImageURL}" data-lightbox="gallery">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" class='gallery'>
+      </a>
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+        <p class="info-item"><b>Views:</b> ${image.views}</p>
+        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+      </div>
+    </div>
+  `;
+
+  galleryElement.insertAdjacentHTML('beforeend', cardHTML);
+
+  const lightbox = new SimpleLightbox('.gallery a', {});
+  lightbox.refresh();
+}
+
+export function displayTotalHits(totalHits) {
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+export function clearGallery() {
+  galleryElement.innerHTML = '';
+}
+
+export function showLoadMoreBtn() {
+  loadMoreBtn.style.display = 'block';
+}
+
+export function hideLoadMoreBtn() {
+  loadMoreBtn.style.display = 'none';
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import axios from 'axios';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import Notiflix from "notiflix";
+import { searchImages } from './search-api.js';
+import * as gallery from './gallery.js';
 
 const form = document.getElementById('search-form');
-const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 let currentPage = 1;
 let searchQuery = '';
@@ -18,71 +15,22 @@ form.addEventListener('submit', async function (event) {
     return;
   }
 
-  clearGallery();
-  currentPage = 1; 
-  await searchImages(searchQuery);
+  gallery.clearGallery();
+  currentPage = 1;
+  await performSearch();
 });
 
 loadMoreBtn.addEventListener('click', async function () {
-  currentPage++;
-  await searchImages(searchQuery, currentPage);
+  await performSearch();
 });
 
-async function searchImages(query, page = 1) {
-  const apiKey = '40892226-0964c3c4ef20d056d0d6e44f5';
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
+async function performSearch() {
   try {
-    const response = await axios.get(apiUrl);
-    const data = response.data;
-
-    if (data.hits.length === 0) {
-      Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-    } else {
-      data.hits.forEach(image => {
-        createImageCard(image);
-      });
-      displayTotalHits(data.totalHits);
-      showLoadMoreBtn();
-      
-    }
+    await searchImages(searchQuery, currentPage);
+    currentPage++;
   } catch (error) {
     console.error('Error fetching images:', error);
   }
 }
-
-function createImageCard(image) {
-  const cardHTML = `
-    <div class="photo-card">
-      <a href="${image.largeImageURL}" data-lightbox="gallery">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy class='galery'">
-      </a>
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${image.likes}</p>
-        <p class="info-item"><b>Views:</b> ${image.views}</p>
-        <p class="info-item"><b>Comments:</b> ${image.comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
-      </div>
-    </div>
-  `;
-
-  gallery.innerHTML += cardHTML;
-
-
-  const lightbox = new SimpleLightbox('.gallery a', {});
-  lightbox.refresh();
-}
-
-function displayTotalHits(totalHits) {
-  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-}
-
-function clearGallery() {
-  gallery.innerHTML = '';
-}
-
-function showLoadMoreBtn() {
-  loadMoreBtn.style.display = 'block';
-}
-
 
