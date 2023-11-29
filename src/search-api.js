bfa358327577be3bac11b67fc4df55f9f82e2935
Anhelina26 <114609780+Anhelina26@users.noreply.github.com>
2023-11-29 16:34:22 +0100

    add changes

--- src/search-api.js
+++ src/search-api.js
@@ -1,3 +1,38 @@
+// import axios from 'axios';
+// import * as gallery from './gallery.js';
+// import Notiflix from 'notiflix';
+
+// let currentPage = 1;
+
+// export async function searchImages(query, page = 1) {
+//   const apiKey = '40892226-0964c3c4ef20d056d0d6e44f5';
+//   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
+
+//   try {
+//     const response = await axios.get(apiUrl);
+//     const data = response.data;
+
+//     if (data.hits.length === 0 && page === 1) {
+//       gallery.hideLoadMoreBtn();
+//       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+//     } else {
+//       if (page === 1) {
+//         gallery.hideLoadMoreBtn();
+//       }
+
+//       data.hits.forEach(image => {
+//         gallery.createImageCard(image);
+//       });
+
+//       gallery.displayTotalHits(data.totalHits);
+//       gallery.showLoadMoreBtn();
+//     }
+//   } catch (error) {
+//     console.error('Error fetching images:', error);
+//     gallery.hideLoadMoreBtn();
+//     Notiflix.Notify.failure('Error fetching images. Please try again later.');
+//   }
+// }
 
 import axios from 'axios';
 import * as gallery from './gallery.js';
@@ -9,15 +44,18 @@ export async function searchImages(query, page = 1) {
   const apiKey = '40892226-0964c3c4ef20d056d0d6e44f5';
   const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`;
 
+  // Explicitly hide the button before making the request
+  gallery.hideLoadMoreBtn();
+
   try {
     const response = await axios.get(apiUrl);
     const data = response.data;
 
     if (data.hits.length === 0 && page === 1) {
-      gallery.hideLoadMoreBtn();
       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
     } else {
       if (page === 1) {
+        // Only hide the button if it's not an incorrect query
         gallery.hideLoadMoreBtn();
       }
 
@@ -30,5 +68,9 @@ export async function searchImages(query, page = 1) {
     }
   } catch (error) {
     console.error('Error fetching images:', error);
+
+    // Ensure the button is hidden in case of an error
+    gallery.hideLoadMoreBtn();
+    Notiflix.Notify.failure('Error fetching images. Please try again later.');
   }
 }
